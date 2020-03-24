151f3ba1f966523602657f36e6f63294b16e1bbe
= <=>
2020-03-24 01:21:05 -0400

    Create HomePage Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+
+import HomePage from './homepage.component'
 import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return <div className="App"><HomePage /></div>;
   }
 }
+
+export default App;
 
